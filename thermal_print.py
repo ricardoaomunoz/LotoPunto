a86147534e43b770b55fdb5bc8b8c97d911c768c
palcort <ricardoaomunoz@gmail.com>
2020-01-15 01:26:53 +0000

    add barcode height

--- thermal_print.py
+++ thermal_print.py
@@ -20,6 +20,48 @@ class ThermalPrinter():
     def close_port(self):
         self.printer_serial.close()   
 
+    #Print data in the print buffer and feeds one line, this commands sets the print
+    #position to the beginning of the line 
+    # LF
+    def print_data_buffer(self):
+      if automatic_line: #CR same as LF when automatic line feed is enabled
+        self.write_serial(13)
+      else:
+        self.write_serial(10)
+
+    # Transmits the selected printer status specified by n in real-time
+    def real_time_status_transmission(self, n):
+      #n=1 printer status, n=2 offline status
+      #n=3 error status, n=4 paper roll sensor status
+      self.write_serial(16, 4 , n)
+
+    #def real_time_request(self):
+
+    #Set the character spacing for the right side of the character
+    #to [n x 0.125 mm - n x 0.0049"]
+    #default n = 0
+    #ESC SP n
+    def set_right_side_character_spacing(self, n):
+      self.write_serial(27, 32 , n)
+
+    #ESC ! n
+    def select_print_mode(self, n):
+      self.write_serial(27, 33, n)
+
+    #Set the distance from the beginning of the line to the 
+    #position at wich subsequent characters are to be printed
+    #the distance from the beginning of the line to the position at
+    #wich subsequencent characters are to be printed
+    #[(nL + nH x 256) x 0.125 mm]
+    # ESC $ nL nH
+    def set_absolute_print_position(self, nL, nH):
+      self.write_serial(27, 36 , nL, nH)
+
+    def setBarcodeHeight(self, val=50):
+      if val < 1: val = 1
+      self.barcodeHeight = val
+      self.write_serial(29, 104, val)
+
     def begin(self, heatTime=defaultHeatTime):
 		self.writeBytes(
 		  27,       # Esco91012# Configure tab stops on recent printers
