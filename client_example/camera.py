ef784628f2afcc376795b02166df953de8215ae3
ricardo <raosoriom.lotopunto@gmail.com>
2020-01-29 02:30:11 +0000

    sys camera client

--- client_example/camera.py
+++ client_example/camera.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.insert(0, '../src/')
-from src.services.camera_service import PicameraControl
+sys.path.insert(0, '../src/services/')
+from camera_service import PicameraControl
 
 cam = PicameraControl()
 cam.preview(120)
