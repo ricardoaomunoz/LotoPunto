261a939b7e5505fbec91eab9e428b6be528ffc76
palcort <ricardoaomunoz@gmail.com>
2020-02-08 06:00:02 +0000

    kivy client

--- kivy/main.py
+++ kivy/main.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+from src.services.camera_service import PicameraControl
 
 
 
@@ -12,6 +13,14 @@ class MyGrid(BoxLayout):
     def btn(self):
         print("botton press")
         self.qr_label.text = "Botton press"
+        cam = PicameraControl()
+        read_from_qr = cam.read_code()
+        if read_from_qr[0]:
+            text = "Read successful: " + str(read_from_qr[1])
+            
+        else:
+            text = "Fail Reading: " + str(read_from_qr[1])
+        self.qr_label.text = text
 
 class TestApp(App):
     def build(self):
