17f4d64e9507f989ca9a3563f684ca8956f127fd
Ricardo <raosoriom.lotopunto@gmail.com>
2020-04-14 19:39:56 -0500

    redis publish

--- src/redis_serv/publish_redis.py
+++ src/redis_serv/publish_redis.py
@@ -2,8 +2,65 @@ from . import r as redis
 import json
 
 def put_insert_money(cash):
-    print("publish money")
-    deliver = redis.publish('money-insert', cash)
-    if deliver == 0:
-        redis.set(name = 'money-insert', value = cash)
-    print(f"delivered to {deliver}")
+    try:
+        print("publish money")
+        data = redis.get('money-insert')
+        if data:
+            data = json.loads(data)
+            data["inserted"] = cash
+            data["credit"] = data["credit"] + cash
+            data["total"] = data["total"] + cash
+        else:
+            data = {
+                "inserted": cash,
+                "credit": cash,
+                "total": cash
+            }
+        data = json.dumps(data)
+        print(f"data to publish: {data}")
+        deliver = redis.publish('money-insert', data)
+        redis.set(name = 'money-insert', value = data)
+        print(f"delivered to {deliver}")
+    except Exception as err:
+        print(f"Error:{err}")
+        pass
+
+def put_message(msg, tipo):
+    try:
+        print("publish msg")
+        if tipo == "error":
+            redis.publish('message-error', msg)
+        elif tipo == "alert":
+            redis.publish('message-alert', msg)
+        elif tipo == "warning":
+            redis.publish('message-warning', msg)
+    except Exception as err:
+        print(f"Error {err}")
+        pass
+
+def put_value_level(ch, route, quantity):
+    try:
+        print("publish value level")
+        data = redis.get("value-level")
+        if data:
+            data = json.loads(data)
+            channel = "channel" + str(ch)
+            data[channel]["route"] = route
+            data[channel]["quantity"] = quantity
+        else:
+            data = {
+                channel: [
+                    {
+                        "route":route,
+                        "quantity":quantity
+                    }
+                ]
+            }
+        data = json.dumps(data)
+        print(f"data to publish: {data}")
+        redis.publish('value-level', data)
+        redis.set(name = 'value-level', value = data)
+
+    except Exception as err:
+        print(f"Error:{err}")
+        pass
